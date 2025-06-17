cd510e8a33b672304f67e71798548fe20e7db69d
SIRXIII <195725515+SIRXIII@users.noreply.github.com>
2025-06-16 22:40:41 -0700

    Updated LenderPortal.tsx

--- src/components/LenderPortal.tsx
+++ src/components/LenderPortal.tsx
@@ -5,9 +5,8 @@ import AuthForm from './AuthForm';
 import LenderDashboard from './LenderDashboard';
 
 function LenderPortal() {
-  const { user, loading } = useAuth();
-  const [isVerified, setIsVerified] = useState(false);
-  const [checkingVerification, setCheckingVerification] = useState(true);
-
-  useEffect(() => {
   return <LenderDashboard />;
+}
+
+export default LenderPortal;
+
